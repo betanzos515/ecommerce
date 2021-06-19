5fd075b02f84b48842d2e3821141aaf942b358b4
David <betanzos515@gmail.com>
2021-06-19 00:11:58 -0500

    Subiendo al repo

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 
+const buttons = document.querySelectorAll('button');
+let contador = 1;
+buttons.forEach(button =>{
+    button.setAttribute('data-id',contador);
+    contador++;
+})
+
+const espacioTarjetas = document.querySelector('.espacio-tarjetas');
+
+espacioTarjetas.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('btn')){
+        console.log(e.target);
+    }
+});
